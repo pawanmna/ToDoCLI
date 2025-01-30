afd957e92a2fbcca2cee2a404cf0cf46fa49c591
pawanmna <pawanmeena200410@gmail.com>
2025-01-30 22:35:19 +0530

    Initial DB implementation.

--- main.go
+++ main.go
@@ -2,6 +2,41 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"time"
 )
+
+func main() {
+	// Ensure environment variables are set correctly
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Parse the flag for task to add
+	task := flag.String("add", "GoGym", "adds task")
+	flag.Parse()
+
+	// Dummy ID (for simplicity)
+	ID := 1
+
+	// Prepare the SQL statement
+	stmt, err := db.Prepare("INSERT INTO tasks (ID, task, status, Created) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmt.Close()
+
+	// Execute the statement
+	_, err = stmt.Exec(ID, *task, 0, time.Now())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Success message
+	fmt.Println("Task added successfully!")
+}
